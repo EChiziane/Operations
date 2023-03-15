543d2e9b94b2f1e3166e0d370ff4fc426632a1c7
EChiziane <eddybruno43@gmail.com>
2023-03-15 21:26:39 +0200

    feat():Adding ICarLoadSerice

---
+++ Back/Application/IServices/ICarLoadService.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Domain;
+
+namespace Application.IServices
+{
+    public interface ICarLoadService
+    {
+        Task<CarLoad> AddCarLoad(CarLoad model);
+        Task<CarLoad> UpdateCarLoad(int clientId, CarLoad model);
+        Task<bool> DeleteCarLoad(int clientId);
+        Task<CarLoad[]> GetAllCarLoadsAsync();
+        Task<CarLoad[]> GetAllCarLoadByNameAsync();
+        Task<CarLoad> GetCarLoadByIdAsync(); 
+    }
+}
