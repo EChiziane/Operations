6422169f55b831abb64db1a32e25170ed342aa68
EChiziane <eddybruno43@gmail.com>
2023-03-15 21:25:04 +0200

    feat(): Adding IDestinationService.cs

---
+++ Back/Application/IServices/IDestinationService.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Domain;
+
+namespace Application.IServices
+{
+    public interface IDestinationService
+    {
+        Task<Destination> AddDestination(Destination model);
+        Task<Destination> UpdateDestination(int clientId, Destination model);
+        Task<bool> DeleteDestination(int clientId);
+        Task<Destination[]> GetAllDestinationsAsync();
+        Task<Destination[]> GetAllDestinationByNameAsync();
+        Task<Destination> GetDestinationByIdAsync(); 
+    }
+}
