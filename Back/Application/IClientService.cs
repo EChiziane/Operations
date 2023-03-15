e4340c8f1bc27e82215083e6713d5e72ab615371
EChiziane <eddybruno43@gmail.com>
2023-03-15 21:23:19 +0200

    feat():Adding IClientService.cs

---
+++ Back/Application/IClientService.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Domain;
+
+namespace Application
+{
+    public interface IClientService
+    {
+        Task<Client> AddClient(Client model);
+        Task<Client> UpdateClient(int clientId, Client model);
+        Task<bool> DeleteClient(int clientId);
+        Task<Client[]> GetAllClientsAsync();
+        Task<Client[]> GetAllClientByNameAsync();
+        Task<Client> GetClientByIdAsync();   
+    }
+}
---
+++ Back/Application/IServices/IMaterialTypeService.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Domain;
+
+namespace Application.IServices
+{
+    public interface IMaterialTypeTypeService
+    {
+        Task<MaterialType> AddMaterialType(MaterialType model);
+        Task<MaterialType> UpdateMaterialType(int materialId, MaterialType model);
+        Task<bool> DeleteMaterialType(int materialId);
+        Task<MaterialType[]> GetAllMaterialTypesAsync();
+        Task<MaterialType[]> GetAllMaterialTypeByNameAsync(string nome);
+        Task<MaterialType> GetMaterialTypeByIdAsync(int materialId);
+    }
+}
