9f73c438aa21d588e7d29eb4f2c24c2900210fa4
EChiziane <eddybruno43@gmail.com>
2023-04-14 04:13:09 +0200

    feat():Creating Clients

---
+++ OperationProject/src/app/Components/client/add-client/add-client.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Client} from "../../../Models/client";
+import {FormGroup} from "@angular/forms";
+import {ClientService} from "../../../services/client.service";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-add-client',
+  templateUrl: './add-client.component.html',
+  styleUrls: ['./add-client.component.css']
+})
+export class AddClientComponent {
+
+  client:Client={
+    id: 0,
+    firstName:'',
+    lastName:'',
+    mobile:''
+  };
+  constructor(private http:HttpClient,
+              private clientService:ClientService,
+              private router: Router) {
+  }
+
+  public createClient():void{
+    this.clientService.addClient(this.client!).subscribe(()=>{
+      this.router.navigate(['/client'])
+    });
+    }
+
+}
